ace6ffe79d8125052c546bdd9b64d1797a683143
ValentinJDT <vjeandot@yahoo.com>
2022-01-31 23:22:36 +0100

    remove skillpoints with useless advancement

--- src/main/java/antikskills/players/events/player/AchievmentEvent.java
+++ src/main/java/antikskills/players/events/player/AchievmentEvent.java
@@ -3,23 +3,24 @@ package antikskills.players.events.player;
 import antikskills.AntikSkills;
 import antikskills.players.AntikPlayer;
 import antikskills.players.AntikPlayerManager;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 
 
 public class AchievmentEvent implements Listener {
 
-    private AntikPlayerManager antikPlayerManager = AntikSkills.getAntikPlayerManager();
+    private final AntikPlayerManager antikPlayerManager = AntikSkills.getAntikPlayerManager();
 
     @EventHandler
     public void onObtain(PlayerAdvancementDoneEvent e) {
+        if(e.getAdvancement().getCriteria().stream().anyMatch(adv -> adv.contains("the_recipe") || adv.contains("has_")))
+            return;
+
         AntikPlayer antikPlayer = antikPlayerManager.getAntikPlayer(e.getPlayer());
         if(antikPlayer == null) return;
 
-        antikPlayer.addExp(250);
+        antikPlayer.addExp(380);
     }
 
 
